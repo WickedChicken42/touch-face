ca11f3867f5b9483d8ce858611df59a51681bda7
James Ullom <jamesullom@jamess-macbook-pro-2.local>
2018-09-26 14:18:42 -0600

    Initial Commit

---
+++ touch-face/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  touch-face
+//
+//  Created by James Ullom on 9/26/18.
+//  Copyright © 2018 Hammer of the Gods Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
